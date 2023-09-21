31b7ceb84604501527b4966359885f1ff1bc5bee
alian <alianalfin18@gmail.com>
2023-09-21 17:40:11 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      h1:['Roboto'],
+      p:['Open Sans'],
+      h2:['Roboto Slabs'],
+      h3:['Averia Libre'],
+    },
+  },
+  plugins: [],
+}
