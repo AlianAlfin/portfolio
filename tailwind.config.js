df9da7e7d28d8ee54ff190f4f7f3bffd8c28da3d
alian <alianalfin18@gmail.com>
2023-09-21 22:57:38 +0800

    cobaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./dist/page/*.{html,js}"],
   theme: {
     extend: {},
     fontFamily: {
